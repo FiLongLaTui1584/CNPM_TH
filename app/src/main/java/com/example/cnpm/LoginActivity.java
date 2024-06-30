b7dce983b8b6640719fee7fe166f0f227996d338
Phi Long <philongt474@gmail.com>
2024-06-30 15:25:35 +0700

    Phi Long commit (đăng xuất, forgot pass, thêm user)

--- app/src/main/java/com/example/cnpm/LoginActivity.java
+++ app/src/main/java/com/example/cnpm/LoginActivity.java
@@ -67,50 +67,50 @@ public class LoginActivity extends AppCompatActivity {
         });
 
 
-//        forgotPassword.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                AlertDialog.Builder builder=new AlertDialog.Builder(LoginActivity.this);
-//                View dialogView=getLayoutInflater().inflate(R.layout.dialog_forgot,null);
-//                EditText emailBox=dialogView.findViewById(R.id.emailBox);
-//
-//                builder.setView(dialogView);
-//                AlertDialog dialog= builder.create();
-//
-//                dialogView.findViewById(R.id.btnReset).setOnClickListener(new View.OnClickListener() {
-//                    @Override
-//                    public void onClick(View v) {
-//                        String userEmail=emailBox.getText().toString();
-//
-//                        if(TextUtils.isEmpty(userEmail) && !Patterns.EMAIL_ADDRESS.matcher(userEmail).matches()){
-//                            Toast.makeText(LoginActivity.this, "Nhập email bạn đã đăng kí!",Toast.LENGTH_SHORT).show();
-//                            return;
-//                        }
-//                        mAuth.sendPasswordResetEmail(userEmail).addOnCompleteListener(new OnCompleteListener<Void>() {
-//                            @Override
-//                            public void onComplete(@NonNull Task<Void> task) {
-//                                if(task.isSuccessful()){
-//                                    Toast.makeText(LoginActivity.this,"Kiểm tra email của bạn", Toast.LENGTH_SHORT).show();
-//                                    dialog.dismiss();
-//                                }else {
-//                                    Toast.makeText(LoginActivity.this,"Gửi thất bại",Toast.LENGTH_SHORT).show();
-//                                }
-//                            }
-//                        });
-//                    }
-//                });
-//                dialogView.findViewById(R.id.btnCancel).setOnClickListener(new View.OnClickListener() {
-//                    @Override
-//                    public void onClick(View v) {
-//                        dialog.dismiss();
-//                    }
-//                });
-//                if(dialog.getWindow() != null){
-//                    dialog.getWindow().setBackgroundDrawable(new ColorDrawable(0));
-//                }
-//                dialog.show();
-//            }
-//        });
+        forgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder=new AlertDialog.Builder(LoginActivity.this);
+                View dialogView=getLayoutInflater().inflate(R.layout.dialog_forgot,null);
+                EditText emailBox=dialogView.findViewById(R.id.emailBox);
+
+                builder.setView(dialogView);
+                AlertDialog dialog= builder.create();
+
+                dialogView.findViewById(R.id.btnReset).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String userEmail=emailBox.getText().toString();
+
+                        if(TextUtils.isEmpty(userEmail) && !Patterns.EMAIL_ADDRESS.matcher(userEmail).matches()){
+                            Toast.makeText(LoginActivity.this, "Nhập email bạn đã đăng kí!",Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        mAuth.sendPasswordResetEmail(userEmail).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if(task.isSuccessful()){
+                                    Toast.makeText(LoginActivity.this,"Kiểm tra email của bạn", Toast.LENGTH_SHORT).show();
+                                    dialog.dismiss();
+                                }else {
+                                    Toast.makeText(LoginActivity.this,"Gửi thất bại",Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                    }
+                });
+                dialogView.findViewById(R.id.btnCancel).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+                if(dialog.getWindow() != null){
+                    dialog.getWindow().setBackgroundDrawable(new ColorDrawable(0));
+                }
+                dialog.show();
+            }
+        });
     }
 
     private void login() {
--- app/src/main/java/com/example/cnpm/MainActivity.java
+++ app/src/main/java/com/example/cnpm/MainActivity.java
@@ -1,14 +1,60 @@
 package com.example.cnpm;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    AppCompatButton btnLogOut, btn_addUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addControls();
+        addEvents();
+    }
+
+    private void addEvents() {
+        btnLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder=new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Đăng xuất");
+                builder.setMessage("Bạn muốn đăng xuất ?");
+                builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent=new Intent(MainActivity.this,LoginActivity.class);
+                        startActivity(intent);
+                    }
+                });
+                builder.setNegativeButton("Hủy", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                builder.create().show();
+            }
+        });
+        btn_addUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(MainActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void addControls() {
+        btnLogOut=findViewById(R.id.btnLogOut);
+        btn_addUser=findViewById(R.id.btn_addUser);
     }
 }
